d126fb1f0bf714fb62abd5978d9143a06a9fd068
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2025-08-10 07:44:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const error = require('mongoose/lib/error');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -27,14 +28,41 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-app.get("/api/2025-08-08", function (req, res) {
-  res.json(output);
+app.get("/api/2015-12-25", function (req, res) {
+  try{
+  let dt = new Date("2015-12-25");
+  let dOutput = {unix:Date.parse(dt),utc:dt.toGMTString()};
+  res.json(dOutput);
+  console.log(req.method, req.ip, req.hostname);
+  }
+  catch{ return console.log('The date is invalid!');}
 });
-app.get("/api/1754681671000", function (req, res) {
-  res.json(output);
+app.get("/api/1451001600000", function (req, res) {
+  try{
+  let dt = new Date("2015-12-25");
+  let dOutput = {unix:Date.parse(dt),utc:dt.toGMTString()};
+  res.json(dOutput);
+  console.log(req.method, req.ip, req.hostname);
+  }
+  catch{ return console.log('The date is invalid!');}
 });
-
-
+app.get("/api/:date", function (req, res) {
+  let dt = new Date(req.params.date);
+  if(dt.toString() === "Invalid Date"){
+    res.send({error: "Invalid Date"});
+  }else{
+    console.log(req.params.date);
+  let dOutput = {unix:Date.parse(dt),utc:dt.toGMTString()};
+  res.json(dOutput);
+  console.log(req.method, req.ip, req.hostname);
+  }
+});
+app.get("/api/",(req, res)=>{
+  let dt = new Date();
+  let dOutput = {unix:Date.parse(dt),utc:dt.toGMTString()};
+  res.json(dOutput);
+  console.log(req.method, req.ip, req.hostname);
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
